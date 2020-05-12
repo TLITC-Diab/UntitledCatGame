c54e4b10e75bd68d924c24f84872bdf984c5cb75
TLITC-Diab <59467981+TLITC-Diab@users.noreply.github.com>
2020-05-12 17:11:31 -0300

    added attack animation

--- UntitledCatGame/Action RPG Resources/Player/Player.gd
+++ UntitledCatGame/Action RPG Resources/Player/Player.gd
@@ -10,7 +10,7 @@ enum{
 	ATTACK
 }
 
-var state
+var state = MOVE
 var velocity = Vector2.ZERO
 
 onready var animationplayer = $AnimaitonPlayer
@@ -20,8 +20,19 @@ onready var animationstate = animationtree.get("parameters/playback")
 func _ready():
 	animationtree.active = true
 
-#movement
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+
+		ROLL:
+			pass
+
+		ATTACK:
+			attack_state()
+
+#movement
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -31,11 +42,21 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationtree.set("parameters/Idle/blend_position", input_vector)
 		animationtree.set("parameters/Run/blend_position", input_vector)
+		animationtree.set("parameters/Attack/blend_position", input_vector)
 		animationstate.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		print(velocity)
 	else:
 		animationstate.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
 	velocity = move_and_slide(velocity)
+
+func attack_state():
+	velocity = Vector2.ZERO
+	animationstate.travel("Attack")
+func attack_animation_finished():
+	state = MOVE
+
 

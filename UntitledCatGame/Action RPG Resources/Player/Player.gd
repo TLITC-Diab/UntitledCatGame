3e31692d96f4da80abf9db297396baef478dafd7
TLITC-Diab <59467981+TLITC-Diab@users.noreply.github.com>
2020-05-11 21:03:43 -0300

    Added smooth movement and acceleration

--- UntitledCatGame/Action RPG Resources/Player/Player.gd
+++ UntitledCatGame/Action RPG Resources/Player/Player.gd
@@ -1,19 +1,24 @@
 extends KinematicBody2D
 
+const FRICTION = 400
+const ACCELERATION = 25
 const MAX_SPEED = 100
 
 var velocity = Vector2.ZERO
 
+
+#movement
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-	print (input_vector)
-	
+	#set acceleration and max speed
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector * MAX_SPEED
+		velocity += input_vector * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_SPEED * delta)
+		#set friction
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	move_and_collide(velocity * delta)
+	move_and_collide(velocity)
